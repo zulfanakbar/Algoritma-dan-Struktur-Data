b6cce639ad6711492211a02e6a1e14d491c71581
zulfanakbar <60720625+zulfanakbar@users.noreply.github.com>
2020-05-14 11:35:56 +0700

    Add files via upload

---
+++ Pertemuan 14/PERTEMUAN 14/Jobset14/src/Tugas6/BinaryTreeArray.java
@@ -0,0 +1,63 @@
+package tugas6;
+
+public class BinaryTreeArray {
+    int[] data;
+    int idxLast;
+    
+    public BinaryTreeArray(int arr, int idxLast){
+        data = new int[arr];
+        this.idxLast = idxLast;
+    }
+    
+    void traverseInOrder(int idxStart){
+        if(idxStart<=idxLast){
+            traverseInOrder(2*idxStart+1);
+            System.out.print(data[idxStart]+" ");
+            traverseInOrder(2*idxStart+2);
+        }
+    }
+    void add(int databr) {
+        if (data[0] == 0) {
+            data[0] = databr;
+        }
+        int idx = 0;
+        
+        while(true) {
+            if(databr < data[idx]){
+                // insert left child
+                if (data[2*idx+1]!=0){
+                    idx = (2*idx)+1;
+                }else{
+                    data[2*idx+1] = databr;
+                    break;
+                }
+            }else if (databr > data[idx]){
+                // insert right child
+                if (data[2*idx+2]==0){
+                    data[2*idx+2] = databr;
+                    break;
+                }else{
+                    idx=2*idx+2;
+                }
+            }else {
+                break;
+            }
+        }
+    }
+
+    void traversePreOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            System.out.print(data[idxStart] + " ");
+            traverseInOrder(2 * idxStart + 1);
+            traverseInOrder(2 * idxStart + 2);
+        }
+    }
+
+    void traversePostOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            traverseInOrder(2 * idxStart + 1);
+            traverseInOrder(2 * idxStart + 2);
+            System.out.print(data[idxStart] + " ");
+        }
+    }
+}
---
+++ Pertemuan 14/PERTEMUAN 14/Jobset14/src/jobset14/Node.java
@@ -0,0 +1,15 @@
+package jobset14;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    
+    public Node() {
+    }
+    public Node(int data) {
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
